73239d4a6e8c2293ce01ee529c5a4074b12fc472
Shauryayamdagni <shauryayamdagni0@gmail.com>
2021-06-19 22:01:36 +0530

    hahahaha

---
+++ week7/week7-grpa-3-merge-the-dictionaries.py
@@ -0,0 +1,7 @@
+git init
+ 1863  git remote add origin "https://github.com/Shauryayamdagni/IITM_python.git"
+ 1864  git remote -v
+ 1865  git add /shaurya/*.py
+ 1866  git add week7
+ 1867  git commit -m "week 7 content"
+ 1868  git push origin master 
